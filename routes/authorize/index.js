1b27379ac39af62e93df0b46444d56245c59df03
Anton Zuev <anzuev@bk.ru>
2015-07-29 23:37:25 +0300

    Изменены апи

--- routes/authorize/index.js
+++ routes/authorize/index.js
@@ -1,10 +1,10 @@
 var checkAuth = require('../../middleware/auth/checkAuth');
 
 module.exports = function(app){
-    app.post('/api/auth/signUp', require('./signUp').post);
-    app.post('/api/auth/signIn', require('./signIn').post);
-    app.post('/api/auth/logout', require('./logout').post);
-    app.get('/api/auth/checkAuth', checkAuth, function(req, res){
+    app.post('/auth/signUp', require('./signUp').post);
+    app.post('/auth/signIn', require('./signIn').post);
+    app.post('/auth/logout', require('./logout').post);
+    app.get('/auth/checkAuth', checkAuth, function(req, res){
         res.send(req.user._id);
         res.end();
     });

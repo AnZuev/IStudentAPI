c71e51aa1ebbb14e5a754c6fe56006b1641b0e35
Anton Zuev <anzuev@bk.ru>
2015-07-30 00:29:23 +0300

    domain part 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 
 module.exports = function(app){
 
-    app.all('/', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    app.all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
     });
 

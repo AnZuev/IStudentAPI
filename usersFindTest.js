07f614ba71a635cb64ce519b9df3e6e4d9efc33a
Anton Zuev <anzuev@bk.ru>
2015-09-07 10:05:37 +0300

    Build 0.0.4
    1) Скрипт создание юзеров

--- usersFindTest.js
+++ usersFindTest.js
@@ -7,7 +7,7 @@ var User = require('./models/User').User;
 var group = 4304;
 var name = 'А';
 var surname = "Зуев";
-
+/*
 User.getPeopleByGroupNumber(group, function(err, result){
     console.log(arguments);
 })
@@ -21,22 +21,21 @@ User.getPeopleByNameAndGroup(surname, group, function(err, result){
 User.getPeopleByNameAndSurnameAndGroup(name, surname, group, function(err, result){
     console.log(arguments);
 })
+*/
 
-/*
-var firstNames = ['Георгий', "Георг", "Антон", "Ант", "Кирилл"];
-var lastNames = ['Попов', "Езеров", "Ильин", "Зуев", "Горбунов"];
+var firstNames = ['Георгий', "Георг", "Антон", "Ант", "Кирилл", "Леша", "Сергей", "Паша"];
+var lastNames = ['Попов', "Езеров", "Ильин", "Зуев", "Горбунов", "Павлюк", "Комаров"];
 var group = 4304;
 var faculty = 'KTI';
 var year = 1;
-var studNumber = 43022310;
+var studNumber = 43029899;
 var password = 'qwerty1234';
 
  for(var i = 0; i < 10; i++){
- User.signUp(firstNames[i%5], lastNames[i%5], group, faculty, year, studNumber+i+1, password, function(err, user){
+ User.signUp(firstNames[i%8], lastNames[i%7], group, faculty, year, studNumber+i+1, password, function(err, user){
  if(err) throw err;
  else{
  console.log(user);
  }
  })
  }
- */

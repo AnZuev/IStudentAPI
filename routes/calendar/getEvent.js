c05bc914d1ae7ca0110789fb53700ff79de2d08a
Anton Zuev <anzuev@bk.ru>
2015-09-09 12:43:10 +0300

    Build 0.0.4(debug)

--- routes/calendar/getEvent.js
+++ routes/calendar/getEvent.js
@@ -15,9 +15,10 @@ exports.get = function(req, res, next){
                 console.log(event.participants.accepted);
                 for(var i = 0; i< event.participants.accepted.length; i++){
                     var newTask = function(callback){
+                        console.log(i);
                         User.getUserById(event.participants.accepted[i], function(err, user){
                             if(err) throw err;
-                            console.log("Вывод из поиска юзера по id " + arguments);
+                            console.log("Вывод из поиска юзера по id " + user);
                             return callback(null, user.personal_information.lastName + " " + user.personal_information.firstName);
                         })
 

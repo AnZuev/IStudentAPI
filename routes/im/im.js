6e91d95827f354a03cba608e3151c9f25e8e2efa
Anton Zuev <anzuev@bk.ru>
2015-09-20 20:15:17 +0300

    Build 0.0.5

--- routes/im/im.js
+++ routes/im/im.js
@@ -1,9 +1,12 @@
 var HttpError = require('../../error').HttpError;
-
+var config = require('../../config');
+var host = config.get('general:host');
 
 
 
 exports.get = function(req, res, next){
-    res.render("im");
+    res.render("im", {
+        host: host
+    });
     next();
 };

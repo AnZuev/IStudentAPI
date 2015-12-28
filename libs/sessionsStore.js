46557b25908242dc78aa421addd73c31b9612c64
Anton Zuev <anzuev@bk.ru>
2015-12-28 23:40:01 +0300

    build 0.0.1(7)
    1) Change connect-mongo requirements

--- libs/sessionsStore.js
+++ libs/sessionsStore.js
@@ -2,7 +2,7 @@ var session = require('express-session');
 var config = require('../config');
 
 
-var MongoStore = require('connect-mongo')(session);
+var MongoStore = require('connect-mongo/es5')(session);
 
 var sessionStore = new MongoStore({url: config.get("mongoose:uri")});
 

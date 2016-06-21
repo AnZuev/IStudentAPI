890370a6698a76faa5759486bc30b3ba96286a4f
Mtdr <paul.osmolovsky@gmail.com>
2016-06-21 15:09:46 +0300

    0.0.2b(8)

---
+++ routes/staticData/workTypes/index.js
@@ -0,0 +1,10 @@
+
+module.exports = function(app){
+    // app.get('/subjects/getSubjects', require('./getSubjects').get);
+    // app.get('/subjects/getAllSubjects', require('./getAllSubjects').get);
+    // app.post('/subjects/addSubject', require('./addSubject').post);
+    // app.post('/subjects/changeName', require('./changeName').post);
+    // app.post('/subjects/removeSubject', require('./removeSubject').post);
+    // app.post('/subjects/activate', require('./activate').post);
+    // app.post('/subjects/deactivate', require('./deactivate').post);
+};
